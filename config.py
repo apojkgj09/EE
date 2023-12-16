26febfe28e5c253cbb4bfb2c9083307ce5c89568
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-12-16 15:09:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,18 +35,18 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ytpremiu/VenomMusic",
+    "https://github.com/apojkgj09/EE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VenomOwners")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Venom_Chatting")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FH_KP")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FH_KN")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "True"))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
 AUTO_SUGGESTION_TIME = int(
     getenv("AUTO_SUGGESTION_TIME", "500"))
